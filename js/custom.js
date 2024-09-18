a196a55e4c1e39ca7374c2e1b3517a9928f59550
MD. SAFIUL HASAN <safiulhasan179@gmail.com>
2024-09-18 17:03:45 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    $('.safi').slick({
+        dots: true,
+        infinite: true,
+        autoplay:true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+
+    //   venobox js start 
+    new VenoBox({
+        selector: '.safiul',
+        numeration: true,
+        infinigall: true,
+        share: true,
+        spinner: 'rotating-plane'
+    });
+
+    new VenoBox({
+        selector: '.hasan',
+    });
+
+    
+    // wow start 
+    new WOW().init();
+
+
+});
